126d4792f6c6d58febb29fa9eace95a07bc53fcd
Nikita <marknik139@gmail.com>
2024-01-28 22:15:37 +0200

    add: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./App.css"
 import Navbar from "./components/NavBar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
       <>
         <Navbar/>
+          <div className="container">
+              <Hero/>
+          </div>
       </>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Hero.css";
+const Hero = () => {
+    return (
+        <section className="hero-container">
+            <div className="hero-content">
+                <h2>Мария Юзефова</h2>
+                <p>
+                    Я являюсь практикующим психологом, специализирующимся в клинической психологии.
+                    Моя задача - поддерживать вас на пути к преодолению эмоциональных трудностей и
+                    достижению гармонии в психологическом благополучии.
+                    С радостью готова помочь вам на этом важном пути.
+                </p>
+            </div>
+
+            <div className="hero-img">
+                <div>
+                    <div className="tech-icon">
+                        <img src="./assets/images/leaf.svg" alt="tech-icon"/>
+                    </div>
+                    <img src="./assets/images/hero-avatar.png" alt="hero-avatar"/>
+                </div>
+
+                <div>
+                    <div className="tech-icon">
+                        <img src="./assets/images/book.svg" alt="tech-icon"/>
+                    </div>
+                    <div className="tech-icon">
+                        <img src="./assets/images/work.svg" alt="tech-icon"/>
+                    </div>
+                    <div className="tech-icon">
+                        <img src="./assets/images/brain.svg" alt="tech-icon"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Hero;

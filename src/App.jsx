47212ed9cbce630e79979f098719d7e68d719d9a
Nikita <marknik139@gmail.com>
2024-02-03 00:22:49 +0200

    add: footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Hero from "./components/Hero/Hero";
 import Skills from "./components/Skills/Skills";
 import Education from "./components/Education/Education";
 import ContactMe from "./components/ContactMe/ContactMe";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
       <>
         <Navbar/>
-          <div className="container">
-              <Hero/>
-              <Skills/>
-              <Education/>
-              <ContactMe/>
-          </div>
+        <div className="container">
+            <Hero/>
+            <Skills/>
+            <Education/>
+            <ContactMe/>
+        </div>
+        <Footer/>
       </>
   )
 }

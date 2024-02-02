c50a0913bb6e9ff9a9e7dea33cbf8e6066bd42a1
Nikita <marknik139@gmail.com>
2024-02-03 00:47:18 +0200

    add: responsive margins

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
             <div className="hero-content">
                 <h2>Ваш психолог</h2>
                 <p>
-                    Здравствуйте! Меня зовут Мария. Я являюсь практикующим психологом, специализирующимся в клинической психологии.
+                    Здравствуйте! Меня зовут Мария. Я практикующий психолог, специализирующийся в клинической психологии.
                     Моя задача - поддерживать вас на пути к преодолению эмоциональных трудностей и
                     достижению гармонии в психологическом благополучии.
                     С радостью готова помочь вам на этом важном пути
@@ -15,22 +15,7 @@ const Hero = () => {
 
             <div className="hero-img">
                 <div>
-                    <div className="tech-icon">
-                        <img src="./assets/images/leaf.svg" alt="tech-icon"/>
-                    </div>
                     <img src="./assets/images/hero-avatar.png" alt="hero-avatar"/>
-                </div>
-
-                <div>
-                    <div className="tech-icon">
-                        <img src="./assets/images/book.svg" alt="tech-icon"/>
-                    </div>
-                    <div className="tech-icon">
-                        <img src="./assets/images/work.svg" alt="tech-icon"/>
-                    </div>
-                    <div className="tech-icon">
-                        <img src="./assets/images/brain.svg" alt="tech-icon"/>
-                    </div>
                 </div>
             </div>
         </section>

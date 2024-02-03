269cf89f48f63db22795a34dd84f29a486ac933d
Nikita <marknik139@gmail.com>
2024-02-03 20:26:30 +0200

    add: price links

--- src/components/Prices/PriceCard/PriceCard.jsx
+++ src/components/Prices/PriceCard/PriceCard.jsx
@@ -4,7 +4,7 @@ import "./PriceCard.css";
 const PriceCard = ({item}) => {
     return <div className="price-card">
         <h6>{item.title}</h6>
-        <div className="price">{item.price}</div>
+        <a href="#contacts" className="price">{item.price}</a>
         <ul>
             {item.details.map((item) => (
                 <li key={item}>{item}</li>

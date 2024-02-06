4eb118bb0897501ea7fd22f51e0cab4b1b1e32e3
Nikita <marknik139@gmail.com>
2024-02-06 21:46:48 +0200

    add: region price

--- src/components/Prices/PriceCard/PriceCard.jsx
+++ src/components/Prices/PriceCard/PriceCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./PriceCard.css";
 
-const PriceCard = ({item}) => {
+const PriceCard = ({item, country}) => {
     return <div className="price-card">
         <h6>{item.title}</h6>
-        <a href="#contacts" className="price">{item.price}</a>
+        <a href="#contacts" className="price">{country === 'IL' ? item.priceIL : item.priceRU}</a>
         <ul>
             {item.details.map((item) => (
                 <li key={item}>{item}</li>
--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Prices.css";
 import {PRICES} from "../../utils/data";
 import PriceCard from "./PriceCard/PriceCard";
+import axios from "axios";
 
 const Prices = () => {
+
+    const [country, setCountry] = useState('RU');
+
+    useEffect(() => {
+        async function fetchMyAPI() {
+            const geoToken = process.env.REACT_APP_GEO_TOKEN;
+            const geoIpAddress = process.env.REACT_APP_GEO_IP;
+            try {
+                let res = await axios.get(`https://ipinfo.io/${geoIpAddress}?token=${geoToken}`);
+                setCountry(res.data.country);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        fetchMyAPI()
+    }, []);
+
     return (
         <section className="prices-container" id="prices">
             <h5>Цены</h5>
             <div className="prices-content">
                 {PRICES.map((item) => (
-                    <PriceCard key={item.title} item={item}/>
+                    <PriceCard key={item.title} item={item} country={country}/>
                 ))}
             </div>
         </section>
